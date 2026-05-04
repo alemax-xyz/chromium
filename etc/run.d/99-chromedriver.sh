eb0de3a313c3e58a9c887f4b5df5e7819d268108
Alexey Maksutov <alexey.maksutov@gmail.com>
2026-05-04 02:59:38 -0400

    updates

--- etc/run.d/99-chromedriver.sh
+++ etc/run.d/99-chromedriver.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
-
-exec sudo -u "$PUSER" -g "$PGROUP" /usr/bin/chromedriver \
+suexec sudo -u "$PUSER" -g "$PGROUP" /usr/bin/chromedriver \
 	--disable-dev-shm-usage \
 	--whitelisted-ips \
 	--allowed-origins=${CHROMEDRIVER_ALLOWED_ORIGINS:-*} \
 	--enable-chrome-logs \
 	--port=${CHROMEDRIVER_PORT:-9515} \
 	--log-level=${CHROMEDRIVER_LOG_LEVEL:-WARNING} \
-	--url-base=${CHROMEDRIVER_URL_BASE:-/}
+	--url-base=${CHROMEDRIVER_URL_BASE:-/} &
